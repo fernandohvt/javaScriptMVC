fc299fe5df0329cfaf916d21458cb98de80c77cf
Fernando Henrique Vieira Trindade <fernando.trindade@ifs.edu.br>
2017-05-23 12:02:34 -0300

    Versão final da curso 1

--- client/js/app/controllers/NegociacaoController.js
+++ client/js/app/controllers/NegociacaoController.js
@@ -1,53 +1,47 @@
 class NegociacaoController {
     
-    constructor(){
-       
+    constructor() {
+        
         let $ = document.querySelector.bind(document);
-
         this._inputData = $('#data');
         this._inputQuantidade = $('#quantidade');
         this._inputValor = $('#valor');
-
+        this._listaNegociacoes = new ListaNegociacoes();
+        
+        this._negociacoesView = new NegociacoesView($('#negociacoesView'));
+        this._negociacoesView.update(this._listaNegociacoes);
+        
+        this._mensagem = new Mensagem();
+        this._mensagemView = new MensagemView($('#mensagemView'));
+        this._mensagemView.update(this._mensagem);
+        
     }
-
+    
     adiciona(event) {
-       
-
+        
         event.preventDefault();
-
-        let data = new Date(
-            ...this._inputData.value
-            .split('-')
-            .map((item, indice ) => item -indice % 2)
-        );
-
-        let negociacao = new NegociacaoController(
-            data ,
+        this._listaNegociacoes.adiciona(this._criaNegociacao());
+        this._negociacoesView.update(this._listaNegociacoes);
+        
+        this._mensagem.texto = 'Negociação adicionada com sucesso';
+        this._mensagemView.update(this._mensagem);
+        
+        this._limpaFormulario();   
+    }
+    
+    _criaNegociacao() {
+        
+        return new Negociacao(
+            DateHelper.textoParaData(this._inputData.value),
             this._inputQuantidade.value,
-            this._inputValor.value
-        )
-
-        
-        let $ = document.querySelector.bind(document);
-        
-        let tabela = $('#tbody');
-       
-        let td = document.createElement('td');
-
-        let trData = document.createElement('tr');
-        trData.textContent = negociacao.data;
-
-        let trQuantidade = document.createElement('tr');
-        trQuantidade.textContent = negociacao.quantidade;
-
-        let trValor = document.createElement('tr');
-        trValor.textContent = negociacao.valor;
-
-        td.appendChild(trData);
-        td.appendChild(trQuantidade);
-        td.appendChild(trValor);
-
-        tabela.appendChild(td);
-
+            this._inputValor.value);    
+    }
+    
+    _limpaFormulario() {
+     
+        this._inputData.value = '';
+        this._inputQuantidade.value = 1;
+        this._inputValor.value = 0.0;
+        this._inputData.focus();   
     }
 }
